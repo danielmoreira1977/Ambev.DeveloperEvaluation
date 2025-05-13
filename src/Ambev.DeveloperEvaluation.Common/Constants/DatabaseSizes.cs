1f6d5123e0c079fdfe13df358e14236b431bcb58
Daniel Moreira <moreira.daniel@live.com>
2025-05-13 11:03:39 -0300

    Fix EF Mapping for user with constants to avoid magic string values

---
+++ src/Ambev.DeveloperEvaluation.Common/Constants/DatabaseSizes.cs
@@ -0,0 +1,11 @@
+﻿namespace Ambev.DeveloperEvaluation.Common.Constants;
+
+public static class DatabaseSizes
+{
+    public static int EmailSize => 100;
+    public static int PasswordSize => 100;
+    public static int PhoneSize => 20;
+    public static int RoleSize => 20;
+    public static int StatusSize => 20;
+    public static int UsernameSize => 50;
+}
--- src/Ambev.DeveloperEvaluation.ORM/Mapping/UserConfiguration.cs
+++ src/Ambev.DeveloperEvaluation.ORM/Mapping/UserConfiguration.cs
@@ -1,7 +1,8 @@
-﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+﻿using Ambev.DeveloperEvaluation.Common.Constants;
+using Ambev.DeveloperEvaluation.Common.Primitives;
+using Ambev.DeveloperEvaluation.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using System.Text.RegularExpressions;
 
 namespace Ambev.DeveloperEvaluation.ORM.Mapping;
 
@@ -12,20 +13,47 @@ public class UserConfiguration : IEntityTypeConfiguration<User>
         builder.ToTable("Users");
 
         builder.HasKey(u => u.Id);
-        builder.Property(u => u.Id).HasColumnType("uuid").HasDefaultValueSql("gen_random_uuid()");
 
-        builder.Property(u => u.Username).IsRequired().HasMaxLength(50);
-        builder.Property(u => u.Password).IsRequired().HasMaxLength(100);
-        builder.Property(u => u.Email).IsRequired().HasMaxLength(100);
-        builder.Property(u => u.Phone).HasMaxLength(20);
+        builder.Property(b => b.Id)
+            .HasConversion(
+                id => id.Value,
+                value => new UserId(value)
+            ).HasColumnName("Id");
 
-        builder.Property(u => u.Status)
-            .HasConversion<string>()
-            .HasMaxLength(20);
+        builder.Property(b => b.Email)
+        .HasConversion(
+            email => email.Value,
+            value => new Email(value)
+        )
+        .IsRequired().HasMaxLength(DatabaseSizes.EmailSize);
+
+        builder.Property(b => b.Password)
+        .HasConversion(
+            password => password.Value,
+            value => new Password(value)
+        )
+        .IsRequired().HasMaxLength(DatabaseSizes.PasswordSize);
+
+        builder.Property(b => b.Phone)
+        .HasConversion(
+            phone => phone.Value,
+            value => new Phone(value)
+        )
+        .IsRequired().HasMaxLength(DatabaseSizes.PhoneSize);
+
+        builder.Property(b => b.Username)
+        .HasConversion(
+            username => username.Value,
+            value => new Username(value)
+        )
+        .IsRequired().HasMaxLength(DatabaseSizes.UsernameSize);
 
         builder.Property(u => u.Role)
             .HasConversion<string>()
-            .HasMaxLength(20);
+            .HasMaxLength(DatabaseSizes.RoleSize);
 
+        builder.Property(u => u.Status)
+            .HasConversion<string>()
+            .HasMaxLength(DatabaseSizes.StatusSize);
     }
 }
